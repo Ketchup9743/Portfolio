406429ba3d653a5b6db01b18d8fb14e249c8abb6
Ketchup9743 <liamlessard20@gmail.com>
2026-09-21 18:02:27 +0000

    resdid whole UI, and colors and just revamped in general

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const themeButton = document.querySelector('#theme');
+const navButtons = document.querySelectorAll('[data-page]');
+
+const navigateTo = (page) => {
+	window.location.href = page;
+};
+
+navButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		navigateTo(button.dataset.page);
+	});
+});
 
 themeButton.addEventListener('click', () => {
 	document.body.classList.toggle('light-theme');
